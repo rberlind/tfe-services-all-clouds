12933e5349b7533afe6dad54dd153f778fe94f88
Roger Berlind <roger@hashicorp.com>
2018-04-26 20:59:12 -0400

    fixed service account outputs

--- modules/service-account-from-provider/outputs.tf
+++ modules/service-account-from-provider/outputs.tf
@@ -1,3 +1,3 @@
 output "secret_name" {
-  value = "${compact(kubernetes_service_account.cats-and-dogs.*.default_secret_name)}"
+  value = "${element(compact(kubernetes_service_account.cats-and-dogs.*.default_secret_name),0)}"
 }
--- modules/service-account-from-yaml/outputs.tf
+++ modules/service-account-from-yaml/outputs.tf
@@ -1,3 +1,3 @@
 output "secret_name" {
-  value = "${compact(data.null_data_source.retrieve_secret_name_from_file.*.outputs["secret_name"])}"
+  value = "${element(compact(data.null_data_source.retrieve_secret_name_from_file.*.outputs["secret_name"]),0)}"
 }
