083bd9675cde815dcba8ca0aaffde866f4107226
Roger Berlind <roger@hashicorp.com>
2018-04-26 20:20:26 -0400

    fixed another output

--- modules/service-account-from-provider/outputs.tf
+++ modules/service-account-from-provider/outputs.tf
@@ -1,3 +1,3 @@
 output "secret_name" {
-  value = "${element(kubernetes_service_account.*.cats-and-dogs.default_secret_name, 0)}"
+  value = "${element(kubernetes_service_account.cats-and-dogs.*.default_secret_name, 0)}"
 }
