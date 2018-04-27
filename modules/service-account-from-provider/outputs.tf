2a3c8250795360c52d6b33b70d24e1aa1163ba6e
Roger Berlind <roger@hashicorp.com>
2018-04-26 20:17:06 -0400

    fixed outputs

--- modules/service-account-from-provider/outputs.tf
+++ modules/service-account-from-provider/outputs.tf
@@ -1,3 +1,3 @@
 output "secret_name" {
-  value = "${kubernetes_service_account.cats-and-dogs.default_secret_name}"
+  value = "${element(kubernetes_service_account.*.cats-and-dogs.default_secret_name, 0)}"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "cats_and_dogs_address" {
-  value = "${module.cats-and-dogs.cats_and_dogs_address}"
+output "cats_and_dogs_address_azure" {
+  value = "${module.cats_and_dogs_azure.cats_and_dogs_address}"
+}
+
+output "cats_and_dogs_address_gcp" {
+  value = "${module.cats_and_dogs_gcp.cats_and_dogs_address}"
+}
+
+output "cats_and_dogs_address_aws" {
+  value = "${module.cats_and_dogs_aws.cats_and_dogs_address}"
 }
