49d57d4d07edc0613924b2cb2ce4d89a0e669fae
Roger Berlind <roger@hashicorp.com>
2018-04-26 22:03:38 -0400

    changed outputs

--- modules/service-account-from-yaml/outputs.tf
+++ modules/service-account-from-yaml/outputs.tf
@@ -1,3 +1,8 @@
 output "secret_name" {
   value = "${data.null_data_source.retrieve_secret_name_from_file.outputs["secret_name"]}"
 }
+
+output "cats_and_dogs_address" {
+  value = "${var.count == 0 ?
+  "" : coalesce("http://cats-and-dogs-frontend.${data.terraform_remote_state.k8s_cluster.master_public_ip}.xip.io", "")}"
+}
